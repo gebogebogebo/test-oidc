9bcaba5c540fd4e8cc788a89327721f73b46d0df
gebo <gebogebogeboge@gmail.com>
2021-07-04 09:08:08 +0900

    update

--- rp/html/secure/welcome.php
+++ rp/html/secure/welcome.php
@@ -3,10 +3,13 @@
   $headers = apache_request_headers();
   foreach ($headers as $header => $value)
   {
-	  if ($header == "OIDC_CLAIM_preferred_username" || $header == "OIDC_CLAIM_email"){
-    		echo "$value <br />\n";
+	  if ($header == "OIDC_CLAIM_preferred_username"){
+    		echo "Username : $value <br />\n";
 	  }
+	  if ($header == "OIDC_CLAIM_email"){
+      echo "Email : $value <br />\n";
   }
+}
   echo "<br /><br />\n";
   echo "<button onclick=\"location.href='./logout.html'\">Logout</button>\n";
 ?>
