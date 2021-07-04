869a4deb7ab74df32da7740e3674fe885e624659
gebo <gebogebogeboge@gmail.com>
2021-07-04 09:38:34 +0900

    update

--- rp/html/secure/welcome.php
+++ rp/html/secure/welcome.php
@@ -8,8 +8,11 @@
 	  }
 	  if ($header == "OIDC_CLAIM_email"){
       echo "Email : $value <br />\n";
+    }
   }
-}
+  
   echo "<br /><br />\n";
-  echo "<button onclick=\"location.href='./logout.html'\">Logout</button>\n";
+  echo "<button onclick=\"location.href='../index.html'\">Return to top while logged in</button>\n";
+  echo "<br /><br />\n";
+  echo "<button onclick=\"location.href='/secure?logout=http://test-oidc-rp:8081/loggedout.html'\">Logout</button>\n";
 ?>
